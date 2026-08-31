7a38eb750f55e972925389e5a8a53bee3e3b8a6b
mithun00gowda <mithun00gowda@gmail.com>
2026-08-31 16:12:01 +0530

    added the getlongestStreak test script to the project

--- test/services/streak_services_test.dart
+++ test/services/streak_services_test.dart
@@ -42,4 +42,54 @@ void main(){
       expect(result, equals(1));
     });
   });
+  
+  group('getLongestStreak', (){
+    test('return 0 for empty input', (){
+      final result = service.getLongestStreak({});
+      expect(result, equals(0));
+    });
+    test('returns 1 for a single day', (){
+      final today = DateTime.now();
+      final normalization = DateTime(today.year,today.month,today.day);
+      final result = service.getLongestStreak({normalization});
+      expect(result, equals(1));
+    });
+    test('finds the longest run even if it is not the most recent', (){
+      final dates = {
+        DateTime(2026,8,1),
+        DateTime(2026,8,2),
+        DateTime(2026,8,3),
+        DateTime(2026,8,4),
+        DateTime(2026,8,7),
+        DateTime(2026,8,8),
+        DateTime(2026,8,9),
+      };
+
+      final results = service.getLongestStreak(dates);
+      expect(results, equals(4));
+    });
+    
+    test('handles dates passes in the ramdoms order', (){
+      final dates = {
+        DateTime(2026,8,3),
+        DateTime(2026,8,1),
+        DateTime(2026,8,2),
+      };
+      final results = service.getLongestStreak(dates);
+      expect(results, equals(3));
+    });
+    test('a single date skipped day resets the run correctly', (){
+      final dates = {
+        DateTime(2026,8,1),
+        DateTime(2026,8,2),
+        DateTime(2026,8,3),
+        DateTime(2026,8,5),
+        DateTime(2026,8,6),
+        DateTime(2026,8,7),
+        DateTime(2026,8,8),
+      };
+      final results = service.getLongestStreak(dates);
+      expect(results, equals(4));
+    });
+  });
 }
